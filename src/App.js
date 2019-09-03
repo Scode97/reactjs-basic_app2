f2b78c4da4e28ebc15e6bc798e19d20d8deebccf
scode97 <sehameldeen1997@gmail.com>
2019-09-03 04:32:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import authors from "./data";
 
 function App() {
+  console.log(authors);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" className="container-fluid">
+      <div className="row">
+        <div className="col-2">
+          <div id="sidebar">
+            <img src="theindex.svg" className="logo" alt="the index logo" />
+            <section>
+              <h4 className="menu-item active">
+                <button>AUTHORS</button>
+              </h4>
+            </section>
+          </div>
+        </div>
+        <div className="content col-10">
+          <div className="authors">
+            <h3>Authors</h3>
+            <div className="row">
+              <div className="col-lg-4 col-md-6 col-12">
+                <div className="card">
+                  <div className="image">
+                    <img
+                      className="card-img-top img-fluid"
+                      src={authors[0].imageUrl}
+                      alt={`${authors[0].first_name} ${authors[0].last_name}`}
+                    />
+                  </div>
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      <span>
+                        {`${authors[0].first_name} ${authors[0].last_name}`}
+                      </span>
+                    </h5>
+                    <small className="card-text">{`${authors[0].books.length} books`}</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 col-12">
+              <div className="card">
+                <div className="image">
+                  <img
+                    className="card-img-top img-fluid"
+                    src={authors[1].imageUrl}
+                    alt={`${authors[1].first_name} ${authors[1].last_name}`}
+                  />
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title">
+                    <span>
+                      {`${authors[1].first_name} ${authors[1].last_name}`}
+                    </span>
+                  </h5>
+                  <small className="card-text">{`${authors[1].books.length} books`}</small>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 col-12">
+              <div className="card">
+                <div className="image">
+                  <img
+                    className="card-img-top img-fluid"
+                    src={authors[2].imageUrl}
+                    alt={`${authors[2].first_name} ${authors[2].last_name}`}
+                  />
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title">
+                    <span>
+                      {`${authors[2].first_name} ${authors[2].last_name}`}
+                    </span>
+                  </h5>
+                  <small className="card-text">{`${authors[2].books.length} books`}</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
